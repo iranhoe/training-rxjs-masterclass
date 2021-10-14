a44d9fce1a4627ba2d5aad7cdad5231e0d9fcf8e
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-10-14 14:58:44 -0700

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,16 @@
-import { Subject } from "./loadingService";
+import { ObservableStore } from "./store";
+
+const store = new ObservableStore({
+    user: 'brian',
+    isAuthenticated: false
+});
+
+store.selectState('user').subscribe(console.log);
+
+store.updateState({
+    user: 'joe'
+});
+
+store.updateState({
+    isAuthenticated: true
+});
--- src/scripts/store.js
+++ src/scripts/store.js
@@ -1,8 +1,35 @@
 
 import { BehaviorSubject } from 'rxjs';
+import { Subject } from 'rxjs';
+import { scan, pluck, distinctUntilKeyChanged } from 'rxjs/operators';
 
 export class ObservableStore {
     constructor(initialState) {
-        this._store = new BehaviorSubject
+        this._store = new BehaviorSubject(
+            initialState
+        );
+        this._stateUpdates = new Subject();
+
+        // accumlate state
+        this._stateUpdates.pipe(
+            scan((acc, curr) => {
+                return { ...acc, ...curr}
+            }, initialState)
+        ).subscribe(this._store);
+    }
+
+    updateState(stateUpdate) {
+        this._stateUpdates.next(stateUpdate);
+    }
+
+    selectState(stateKey) {
+        return this._store.pipe(
+            distinctUntilKeyChanged(stateKey),
+            pluck(stateKey)
+        );
+    }
+
+    stateChanges() {
+        return this._store.asObservable();
     }
 }
